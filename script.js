193c54ce3d061f2f8487675152c1057c5f0a0983
ZUNKANYDO <zunkanydo@gmail.com>
2026-05-26 12:24:04 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("ZUNKANYDO Portfolio Loaded");
+
+/* FORMULARIO */
+
+document
+.getElementById("contact-form")
+.addEventListener("submit", function(e){
+
+e.preventDefault();
+
+alert(
+"Gracias por contactarme 🚀\n\nPróximamente conectaré este formulario a EmailJS."
+);
+
+});
